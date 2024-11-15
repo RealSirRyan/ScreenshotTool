47924c87d61755b87ecb495f96a6d0520e0de4a3
Ryan <ryan8sorensen@gmail.com>
2024-11-15 11:14:29 -0800

    Added multiple screenshot types, and screenshots now get added to the clipboard

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ScreenshotTool
-Screenshot Tool for Linux
+Simple screenshot tool for Linux X11 written in python.
+
+Screenshots are saved in a directory named after the app they are from
+
+Arguments:
+- 'desktop' - takes a screenshot of the whole desktop
+- 'window' - takes a screenshot of the active window
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from Xlib import display
-from PIL import Image
 import pyautogui
 import os
 import datetime
 import fnmatch
 import subprocess
 import io
+import sys
 
 
 #Get the focus window
@@ -23,6 +23,10 @@ def get_focus_window():
             window = window.query_tree().parent
 
 def get_geometry(focus_window):
+    
+    screen_width = pyautogui.size().width
+    screen_height = pyautogui.size().height
+    
     child_geometry = focus_window.get_geometry()
     parent_geometry = focus_window.query_tree().parent.get_geometry()
 
@@ -35,36 +39,62 @@ def get_geometry(focus_window):
     if y < 0:
         height += y
         y = 0
+    
+    if x + width > screen_width:
+        width = screen_width - x
+    if y + height > screen_height:
+        height = screen_height - y
 
     return x, y, width, height
 
-
-
-
 def main():
+
+    '''
+    Screenshot types:
+        window
+        desktop
+    '''
+    #Getting arguments
+    args = sys.argv
+    print(args)
+    if len(args) < 2:
+        screenshot_type = 'window'
+    else:
+        screenshot_type = args[1]
 
     overrides = {
         'minecraft*': 'Minecraft', #Minecraft adds the version number to end. This way, there won't be a different directory for each version
         'nemo-desktop': '',
     }
 
-    screenshots_dir = os.path.expanduser('~/Desktop/screenshots')
+    screenshots_dir = os.path.expanduser('~/Pictures/Screenshots')
     
-    #Todo: This assumes focus_window exists
-    focus_window = get_focus_window()
     
-    app_name = focus_window.get_wm_class()[1]
-    x, y, width, height = get_geometry(focus_window)
-    
-    print('App: ', app_name)
-    print(f'Geometry: ({x}, {y}, {width}, {height})')
+    #Do something different depending on what screenshot type it is
+    if screenshot_type == 'window':
+        #Todo: This assumes focus_window exists
+        focus_window = get_focus_window()
+        
+        app_name = focus_window.get_wm_class()[1].capitalize()
+        x, y, width, height = get_geometry(focus_window)
+        
+        print('App: ', app_name)
+        print(f'Geometry: ({x}, {y}, {width}, {height})')
 
-    #Check if there is an override for app_name
-    for old, new in overrides.items():
-        if fnmatch.fnmatch(app_name.lower(), old.lower()):
-            app_name = new
-            print('App name override: ', app_name)
-            break
+        #Check if there is an override for app_name
+        for old, new in overrides.items():
+            if fnmatch.fnmatch(app_name.lower(), old.lower()):
+                app_name = new
+                print('App name override: ', app_name)
+                break
+    
+    elif screenshot_type == 'desktop':
+        x, y, width, height = 0, 0, pyautogui.size().width, pyautogui.size().height
+        app_name = ''
+   
+    else:
+        raise ValueError('Invalid screenshot type')
+    
 
     #Take a screenshot
     screenshot = pyautogui.screenshot(region=(x, y, width, height))
@@ -74,10 +104,10 @@ def main():
     screenshot.save(f'{screenshots_dir}/{app_name}/{file_name}.png')
 
     #Copy to clipboard
-    '''image_bytes = io.BytesIO()
-    screenshot.save(image_bytes, format='PNG')
-    image_bytes.seek(0)
-    subprocess.run(['xclip', '-selection', 'clipboard', '-t', 'image/png', '-i'], input=image_bytes.read())'''
+    image_bytes = io.BytesIO()
+    screenshot.save(image_bytes, format='PNG')#Save to BytesIO object (in memory)
+    image_bytes.seek(0)#Move to beginning of BytesIO object
+    subprocess.run(['xclip', '-selection', 'clipboard', '-t', 'image/png', '-i'], input=image_bytes.read())#Copy to clipboard
 
 if __name__ == "__main__":
     main()
